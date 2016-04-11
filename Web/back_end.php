27d8ffbd0dc07fbf3e1556c03903b7b7c33393e8
Megan Yates <mey29@pitt.edu>
2016-04-11 15:01:23 -0400

    Sortable, Draggable, Droppable to create tab

--- Web/back_end.php
+++ Web/back_end.php
@@ -35,25 +35,7 @@
 		function Start() {
 			refreshPage();
 		}
-	
-// Drag and drop
-// 		function dragAndDropTableInit() {
-// 			var table = document.getElementById('pickupTable');
-// 			var tableDnD = new TableDnD();
-// 			tableDnD.init(table);
-// 		}
-// 	
-// 	
-// 		function wrap(top, selector, bottom) {
-// 			var matches = document.querySelectorAll(selector);
-// 			for (var i = 0; i < matches.length; i++){
-// 				var modified = top + matches[i].outerHTML + bottom;
-// 				matches[i].outerHTML = modified;
-// 				console.log(modified);
-// 			}
-// 		}
-		
-	
+
 		function nextPickup() {
 			var httpRequest;
 
@@ -80,11 +62,9 @@
 
 			var type = 2; 
 			var T = document.getElementById("pickupTable");
-// 			var id = T.rows[1].id;
 			var id = T.getElementsByTagName("li")[0].className;
-			var item = removeRowFromPickupList(id);			
+			var item = removeRowFromPickupTable(id);			
 			displayData(item);
-			// T.deleteRow(1);
 			jQuery("#pickupTable li:first-child").remove();
 
 
@@ -124,13 +104,11 @@
 
 			var type = 3; 
 			var T = document.getElementById("dropoffTable");
-			// var id = T.rows[1].id;
 			var id = T.getElementsByTagName("li")[0].className;
 			console.log(id);
-			var item = removeRowFromDropOffList(id);
+			var item = removeRowFromDropOffTable(id);
 			displayData(item);
 			jQuery("#dropoffTable li:first-child").remove();
-// 			T.deleteRow(1);
 
 			var data = 'type=' + type + '&id=' + id;
 		
@@ -170,7 +148,7 @@
 			var T = document.getElementById("talkTable");
 			var id = T.getElementsByTagName("li")[0].className;
 			console.log(id);
-			var item = removeRowFromTalkList(id);
+			var item = removeRowFromTalkTable(id);
 			displayData(item);
 			jQuery("#talkTable li:first-child").remove();
 
@@ -327,7 +305,7 @@
 			}
 		}
 	
-		function removeRowFromPickupList(id) {
+		function removeRowFromPickupTable(id) {
 			var ret = 0;
 			for (var i = 0; i < pickupQueue.length; i++) {
 				if (pickupQueue[i].id == id) {
@@ -340,7 +318,7 @@
 			return ret;
 		}
 	
-		function removeRowFromDropOffList(id) {
+		function removeRowFromDropOffTable(id) {
 			var ret = 0;
 			for (var i = 0; i < dropoffQueue.length; i++) {
 				if (dropoffQueue[i].id == id) {
@@ -353,7 +331,7 @@
 			return ret;
 		}
 		
-		function removeRowFromTalkList(id) {
+		function removeRowFromTalkTable(id) {
 			var ret = 0;
 			for (var i = 0; i < talkQueue.length; i++) {
 				if (talkQueue[i].id == id) {
@@ -367,98 +345,6 @@
 		}
 
 		function showQueueTable() {
-// 			var P = document.getElementById("pickup");
-// 			var D = document.getElementById("dropoff");
-// 			var Q = document.getElementById("talk");
-// 			var pParent = P.parentNode;
-// 			var dParent = D.parentNode;
-// 			var qParent = Q.parentNode;
-// 			console.log(pParent);
-// 			console.log(dParent);
-// 			console.log(qParent);
-			// var newPT = document.createElement('table');
-// 			newPT.setAttribute('id', 'pickupTable');
-//  		newPT.border = 1;
-// 			newPT.className = 'pickuptable';
-// 			var hprow = newPT.insertRow(0);
-// 			hprow.align = 'left';
-// 			var newPT = document.createElement('ul');
-// 			newPT.setAttribute('id', 'pickupTable');
-// 			newPT.className = 'pickuptable';
-		
-// 			var newDT = document.createElement('table');
-// 			newDT.setAttribute('id', 'dropoffTable');
-//  		newDT.border = 1;
-// 			newDT.className = 'dropofftable';
-// 			var hdrow = newDT.insertRow(0);
-// 			hdrow.align = 'left';
-// 			var newDT = document.createElement('ul');
-// 			newDT.setAttribute('id', 'dropoffTable');
-// 			newDT.className = 'dropofftable';
-// 			
-// 			var newQT = document.createElement('table');
-// 			newQT.setAttribute('id', 'talkTable');
-// 			newQT.border = 1;
-// 			newQT.className = 'talktable';
-// 			var hqrow = newQT.insertRow(0);
-// 			hqrow.align = 'left';
-// 			var newQT = document.createElement('ul');
-// 			newQT.setAttribute('id', 'talkTable');
-// 			newQT.className = 'talktable';
-		
-// 			var currCell = hprow.insertCell(0);
-// 			var currCell1 = hdrow.insertCell(0);
-// 			var currCell2 = hqrow.insertCell(0);
-// 			var contents = document.createTextNode('Type');
-// 			var contents1 = document.createTextNode('Type');
-// 			var contents2 = document.createTextNode('Type');
-// 			currCell.appendChild(contents);
-// 			currCell1.appendChild(contents1);
-// 			currCell2.appendChild(contents2);
-
-// 			var currCell = hprow.insertCell(1);
-// 			var currCell1 = hdrow.insertCell(1);
-// 			var currCell2 = hqrow.insertCell(1);
-// 			contents = document.createTextNode('First Name');
-// 			contents1 = document.createTextNode('First Name');
-// 			contents2 = document.createTextNode('First Name');
-// 			currCell.appendChild(contents);
-// 			currCell1.appendChild(contents1);
-// 			currCell2.appendChild(contents2);
-
-// 			var currCell = hprow.insertCell(1);
-// 			var currCell1 = hdrow.insertCell(1);
-// 			var currCell2 = hqrow.insertCell(1);
-// 			contents = document.createTextNode('Last Name');
-// 			contents1 = document.createTextNode('Last Name');
-// 			contents2 = document.createTextNode('Last Name');
-// 			currCell.appendChild(contents);
-// 			currCell1.appendChild(contents1);
-// 			currCell2.appendChild(contents2);
-
-// 			var currCell = hprow.insertCell(3);
-// 			var currCell1 = hdrow.insertCell(3);
-// 			contents = document.createTextNode('Date of Birth');
-// 			contents1 = document.createTextNode('Date of Birth');
-// 			currCell.appendChild(contents);
-// 			currCell1.appendChild(contents1);
-
-// 			var currCell1 = hdrow.insertCell(4);
-// 			contents1 = document.createTextNode('Relation');
-// 			currCell1.appendChild(contents1);
-// 
-// 			var currCell = hprow.insertCell(4);
-// 			contents = document.createTextNode('Returning Customer');
-// 			currCell.appendChild(contents);
-// 
-// 			var currCell1 = hdrow.insertCell(5);
-// 			contents1 = document.createTextNode('Allergies');
-// 			currCell1.appendChild(contents1);
-
-// 			pParent.replaceChild(newPT, P);
-// 			dParent.replaceChild(newDT, D);
-// 			qParent.replaceChild(newQT, Q);
-
 			for (var i = 0; i < pickupQueueCount; i++) {
 				addRow(pickupQueue[i].id, pickupQueue[i].type, pickupQueue[i].refill, pickupQueue[i].first_name, pickupQueue[i].last_name, pickupQueue[i].middle, pickupQueue[i].date_of_birth, pickupQueue[i].gender, pickupQueue[i].position, pickupQueue[i].home_address, pickupQueue[i].city, pickupQueue[i].state, pickupQueue[i].zip, pickupQueue[i].phone, pickupQueue[i].phone_type, pickupQueue[i].notifications, pickupQueue[i].allergies_list, pickupQueue[i].current_meds, pickupQueue[i].signature, pickupQueue[i].date, pickupQueue[i].relation, pickupQueue[i].returning_customer, pickupQueue[i].insurance_card_number, pickupQueue[i].allergies);
 			}
@@ -468,12 +354,6 @@
 			for (var i = 0; i < talkQueueCount; i++) {
 				addRow(talkQueue[i].id, talkQueue[i].type, talkQueue[i].refill, talkQueue[i].first_name, talkQueue[i].last_name, talkQueue[i].middle, talkQueue[i].date_of_birth, talkQueue[i].gender, talkQueue[i].position, talkQueue[i].home_address, talkQueue[i].city, talkQueue[i].state, talkQueue[i].zip, talkQueue[i].phone, talkQueue[i].phone_type, talkQueue[i].notifications, talkQueue[i].allergies_list, talkQueue[i].current_meds, talkQueue[i].signature, talkQueue[i].date, talkQueue[i].relation, talkQueue[i].returning_customer, talkQueue[i].insurance_card_number, talkQueue[i].allergies);
 			}
-		
-			/* // Drag and drop
-			hrow.setAttribute("NoDrag", "1");
-			hrow.setAttribute("NoDrop", "1");
-			dragAndDropTableInit();
-			*/
 		}
 
 		function addRow(id, type, refill, first_name, last_name, middle, date_of_birth, gender, position, home_address, city, state, zip, phone, phone_type, notifications, allergies_list, current_meds, signature, date, relation, returning_customer, insurance_card_number, allergies) {
@@ -489,92 +369,23 @@
 		
 			var li = document.createElement("li");
 			li.id = "ui-state-default";
-
-// 			li.style.background = "#e6dbbe";
-// 			li.style.margin = "0 5px 5px 5px";
-// 			li.style.padding = "5px";
-
 			li.className = id;
 		
 			if (type == 'pickup') {
-// 				var C = R.insertCell(0);
-// 				var txt = document.createTextNode(type);
-// 				C.appendChild(txt);
-			
-// 				C = R.insertCell(1);
-// 				txt = document.createTextNode(first_name);
-// 				C.appendChild(txt);
-			
-// 				C = R.insertCell(0);
-// 				txt = document.createTextNode(last_name);
-// 				C.appendChild(txt);
 				li.appendChild(document.createTextNode(last_name));
 				T.appendChild(li);
-
-// 				C = R.insertCell(3);
-// 				if(date_of_birth == "0000-00-00")
-// 					txt = document.createTextNode("Database");
-// 				else
-// 					txt = document.createTextNode(date_of_birth);
-// 				C.appendChild(txt);
-// 			
-// 				C = R.insertCell(4);
-// 				txt = document.createTextNode(returning_customer);
-// 				C.appendChild(txt);
 			}
 			else if (type == 'returningdropoff' || type == 'newdropoff'){
 				if (type == 'returningdropoff') {				
-				// 	C = R.insertCell(0);
-// 					txt = document.createTextNode(last_name);
-// 					C.appendChild(txt);
-				li.appendChild(document.createTextNode(last_name));
-				T.appendChild(li);
+					li.appendChild(document.createTextNode(last_name));
+					T.appendChild(li);
 				}
 				else {
-					// C = R.insertCell(0);
-// 					var span = document.createElement('span');
-// 					span.setAttribute('class',"ui-icon ui-icon-notice");
-// 					$(".ui-icon ui-icon-notice").text(last_name);
-// 					C.appendChild(span);
-				li.appendChild(document.createTextNode(last_name));
-				T.appendChild(li);
+					li.appendChild(document.createTextNode(last_name));
+					T.appendChild(li);
 				}
-				
-// 				var C = R.insertCell(1);
-// 				var txt = document.createTextNode(type);
-// 				C.appendChild(txt);
-				
-// 				C = R.insertCell(1);
-// 				txt = document.createTextNode(first_name);
-// 				C.appendChild(txt);
-
-// 				C = R.insertCell(3);
-// 				txt = document.createTextNode(date_of_birth);
-// 				C.appendChild(txt);
-// 			
-// 				C = R.insertCell(4);
-// 				txt = document.createTextNode(relation);
-// 				C.appendChild(txt);
-// 			
-// 				C = R.insertCell(5);
-// 				if(allergies == "1")
-// 					txt = document.createTextNode("yes");
-// 				else
-// 					txt = document.createTextNode("no");
-// 				C.appendChild(txt);
 			}
 			else {
-// 				var C = R.insertCell(0);
-// 				var txt = document.createTextNode(type);
-// 				C.appendChild(txt);
-			
-// 				C = R.insertCell(1);
-// 				txt = document.createTextNode(first_name);
-// 				C.appendChild(txt);
-			
-				// C = R.insertCell(0);
-// 				txt = document.createTextNode(last_name);
-// 				C.appendChild(txt);
 				li.appendChild(document.createTextNode(last_name));
 				T.appendChild(li);
 			}
@@ -605,9 +416,6 @@
 			}
  
 			var type = 1; 
-// 			var pickupRows = document.getElementById("pickupTable").rows.length-1;
-// 			var dropoffRows = document.getElementById("dropoffTable").rows.length-1;
-// 			var talkRows = document.getElementById("talkTable").rows.length-1;
 			var pickupRows = document.getElementById("pickupTable").length-1;
 			var dropoffRows = document.getElementById("dropoffTable").length-1;
 			var talkRows = document.getElementById("talkTable").length-1;
@@ -640,24 +448,25 @@
 			$( "ul, li" ).disableSelection();
 		  });
 		
-	// 	$(function() {
-//     		$( "#pickupTable" ).draggable({revert: true});
-//     		$( "#dropoffTable" ).draggable({revert: true});
-//     		$( "#talkTable" ).draggable({revert: true});
-//   		});  
-// 		 
-// 		$(function() {
-//     		$( "#tabs" ).droppable({
-//       			activeClass: "ui-state-highlight",
-//       			drop: function( event, ui ) {
-//       				var draggableId = $(ui.draggable).attr("class");
-//       				next(draggableId);
-//              		$(ui.draggable).remove()
-//         		}
-//     		});
-//   		});
+	$(function() {
+    		$( "#pickupTable" ).draggable({revert: true});
+    		$( "#dropoffTable" ).draggable({revert: true});
+    		$( "#talkTable" ).draggable({revert: true});
+  		});  
+		 
+		$(function() {
+    		$( "#tabs" ).droppable({
+      			activeClass: "ui-state-highlight",
+      			drop: function( event, ui ) {
+      				var draggableId = ui.draggable.attr('class').split(" ")[0];
+      				var table = ui.draggable.parent().attr('id');
+      				next(draggableId, table);
+             		$(ui.draggable).remove()
+        		}
+    		});
+  		});
   			
-		function next(dropId) {
+		function next(dropId, table) {
 			var httpRequest;
 
 			if (window.XMLHttpRequest) { // Mozilla, Safari, ...
@@ -680,13 +489,21 @@
 			if (!httpRequest) {
 				alert('Cannot create an XMLHTTP instance');
 			}
-			alert(dropId);
-			var item = removeRowFromPickupList(dropId);
-			displayData(item);
-			jQuery("#pickupTable li:first-child").remove();
-
-			var data = 'type=' + type + '&id=' + dropId;
-		
+			if(table.includes('pickup')){
+				var item = removeRowFromPickupTable(dropId);
+				displayData(item);
+				var data = 'type=pickup' + '&id=' + dropId;
+			}
+			if(table.includes('dropoff')){
+				var item = removeRowFromDropOffTable(dropId);
+				displayData(item);
+				var data = 'type=dropoff' + '&id=' + dropId;
+			}
+			if(table.includes('talk')){
+				var item = removeRowFromTalkTable(dropId);
+				displayData(item);
+				var data = 'type=talk' + '&id=' + dropId;
+			}
 			// comment this out to disable database connection for easier testing
 			//httpRequest.open('POST', 'back_end_php.php', true);
 			//httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
